94dfb19b099db4dbc7a6afbb280461e05fcde1bd
Swapnxll <130583218+Swapnxll@users.noreply.github.com>
2026-07-25 12:04:54 +0530

    frontend added-1

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,6 +6,6 @@ import { Toaster } from "react-hot-toast";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <Toaster position="top" />
+    <Toaster position="top-center" />
   </StrictMode>,
 );
